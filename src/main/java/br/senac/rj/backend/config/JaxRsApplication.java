8e280c8cbfe5356740181757863ca8d52da6b940
eliakim-ads <eliakim.ldo@gmail.com>
2025-11-18 21:03:52 -0300

    Exclusão Classe Produtor.

--- src/main/java/br/senac/rj/backend/config/JaxRsApplication.java
+++ src/main/java/br/senac/rj/backend/config/JaxRsApplication.java
@@ -22,7 +22,6 @@ public class JaxRsApplication extends Application {
 		resources.add(br.senac.rj.backend.controller.UsuarioController.class);
 		resources.add(br.senac.rj.backend.controller.EventoController.class);
 		resources.add(br.senac.rj.backend.controller.IngressoController.class);
-		resources.add(br.senac.rj.backend.controller.ProdutorController.class);
 		resources.add(br.senac.rj.backend.controller.CupomController.class);
 		resources.add(br.senac.rj.backend.filter.AuthFilter.class);
 		return resources;
